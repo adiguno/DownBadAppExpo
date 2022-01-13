5745e32e19d3d66347d35bf3e21c8ff2e97352b0
adiguno <guodian44@gmail.com>
2022-01-12 23:55:25 -0500

    add clear text on focus, and next page button

--- CreateHabit.tsx
+++ CreateHabit.tsx
@@ -1,23 +1,59 @@
 import React, { Component } from "react";
-import { Text, TextInput, View, StyleSheet } from "react-native";
+import {
+  Image,
+  Text,
+  TextInput,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
 
 export function CreateHabit() {
   const [text, onChangeText] = React.useState(
     "Tell me why ain't nothin' but a heartache"
   );
 
+  const [isSubmitted, onSubmit] = React.useState(false);
+
   return (
     <View style={styles.container}>
       <Text>Why ya down bad bro?</Text>
       <TextInput
+        onFocus={clearText}
         onSubmitEditing={submitHabit}
         onChangeText={onChangeText}
         value={text}
       />
+
+      <CheckMark></CheckMark>
     </View>
   );
 
+  function CheckMark() {
+    if (isSubmitted) {
+      return (
+        <TouchableOpacity onPress={goToNext} style={styles.roundButton1}>
+          <View>
+            <Image
+              source={require("./assets/checkmark.png")}
+              resizeMode="contain"
+              style={styles.checkmark}
+            />
+          </View>
+        </TouchableOpacity>
+      );
+    }
+    return null;
+  }
+
+  function goToNext() {
+    console.log(`goToNext text: ${text}`);
+  }
+  function clearText() {
+    onChangeText("");
+  }
   function submitHabit() {
+    onSubmit(true);
     console.log(`submitHabit text: ${text}`);
     console.log("Submit somethinthing");
   }
@@ -28,5 +64,18 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  roundButton1: {
+    width: 100,
+    height: 100,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 10,
+    borderRadius: 100,
+    backgroundColor: "#8099CC",
+  },
+  checkmark: {
+    width: 100,
+    height: 100,
   },
 });
